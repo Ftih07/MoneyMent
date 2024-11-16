bce572a895557d222c8abdabba86d2f8cdd9f750
Ftih07 <naufalfathi37@gmail.com>
2024-11-16 14:50:51 +0700

    home page

---
+++ app/src/main/java/com/naufal/moneyment/Home.kt
@@ -0,0 +1,50 @@
+package com.naufal.moneyment
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
+
+class Home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.r3lfy43g4fk3))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rdd5bh4qdo4))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rhgqp0k0vnpg))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.ro9i29uldxba))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rjrvf2nqd7ge))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rbz49ikwxmll))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rgyfboadb8h))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rbg7o8bh2yk7))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rf4wltrmutr4))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rk04dpu9e8g))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rvo4gp72z25c))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.r0sfzs1wq3t2a))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rpxdu562lhy))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rky9tmfxa1))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rvbkm2myuzrn))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rknsmxusrgfo))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.r6f9fptj7wxc))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.r96jekixx6a))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.r472g7311vrg))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rsfm8sb4vddo))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.r2obji6aq57u))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rd5674jj29rw))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rxigglvwz42))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.r44g89iy2hj5))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.r6broh5jj6r))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.r0rtzfifmsgdo))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.ria3d5gpseyr))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rpvspyotpxi))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.r0xqeibpgee0i))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rp5dc9t6o28q))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.r98dyodzyg7g))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rysbgu0tui5r))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rwmy8zsxwe2))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rm3d7wuthzyg))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.r9bls08cg9fb))
+    }
+}
